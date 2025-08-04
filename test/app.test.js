457b073a5c4a2d962b7f623afd856106fdc31072
Aasif mohd <122384084+Mohdaasif97@users.noreply.github.com>
2025-08-04 13:28:29 +0200

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,11 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('GET /', () => {
-  it('responds with Hello, CI/CD!', async () => {
+  it('should respond with welcome HTML content', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello, CI/CD!');
+    expect(response.text).toContain('Hi, welcome to this site!');
+    expect(response.text).toContain('Aasif Mohd');
+    expect(response.text).toContain('<!DOCTYPE html>');
   });
 });
