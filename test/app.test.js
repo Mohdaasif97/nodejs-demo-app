5c8712c1124a78b1c193c10dce07924516bb405d
Aasif mohd <122384084+Mohdaasif97@users.noreply.github.com>
2025-08-04 13:58:28 +0200

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,16 @@
 const request = require('supertest');
 const app = require('../app');
+let server;
 
+beforeAll(() => {
+  // If your app doesn't export the server, you might need to modify your app.js
+  server = app.listen(0); // 0 means random available port
+});
+
+afterAll(async () => {
+  // Properly close the server after all tests
+  await server.close();
+});
 describe('GET /', () => {
   it('should respond with welcome HTML content', async () => {
     const response = await request(app).get('/');
